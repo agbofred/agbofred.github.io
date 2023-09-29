f3c963a6e7122ce1d1bb551007595aa2126c3b58
agbofred <93585348+agbofred@users.noreply.github.com>
2023-09-28 19:40:37 -0700

    Ch_4_2_F23 updated

--- Class_Demos/demo_22_9.py
+++ Class_Demos/demo_22_9.py
@@ -1,3 +1,15 @@
+import pgl
+import english
+import random
 a = "anything"
 b = 3900000
-print(f'I am trying to use f string {a:^20} and {b:,.2f}')
+#print(f'I am trying to use f string {a:^20} and {b:,.2f}')
+
+a, b= 2,10
+#print(random.randint(a,b))
+#print(random.randrange(10))
+#print(random.random())
+
+li =["Fred", "Class", "CS151", "Ford"]
+
+print(random.choice(li))
--- Class_Demos/demo_25_9.py
+++ Class_Demos/demo_25_9.py
@@ -1,4 +1,45 @@
-s = "ABCdef"
+import random
+from pgl import GArc, GWindow, GRect, GLabel, GLine, GOval
+GWINDOW_WIDTH = 400
+GWINDOW_HEIGHT = 400
+
+def draw_over():
+    gw = GWindow(GWINDOW_WIDTH,GWINDOW_HEIGHT)
+    message =GLabel("Hello Graphics", 100, 100)
+    oval = GOval(20,20,350,350)
+    
+    oval.set_filled(True)
+    oval.set_fill_color("yellow")
+    lefteye = GOval(100,100, 30,30)
+    lefteye.set_filled(True)
+    righteye = GOval(250,100, 30,30)
+    righteye.set_filled(True)
+    mouth = GLine(150,220, 220, 220)
+    mouth.set_line_width(4)
+    #gw.add(message)
+    gw.add(oval)
+    gw.add(lefteye)
+    gw.add(righteye)
+    gw.add(mouth)
+
+def draw_checker():
+    gw = GWindow(200,200)
+    for c in range(0,10):
+        for r in range(0,10):
+            rect = GRect(20*c,20*r,20,20)
+            if (r+c) % 2 != 0:
+                rect.set_filled(True)
+            gw.add(rect)
+
+
+if __name__ == "__main__":
+    draw_over()
+    #draw_checker()
+
+
+
+"""s = "ABCdef"
 for ch in s:
     if s.rfind(ch)>0:
         print(ch)
+"""
