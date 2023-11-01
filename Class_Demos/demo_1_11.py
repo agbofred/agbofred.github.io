b524eb0d33d2eb14dc22b5114d6764500f8831a5
agbofred <93585348+agbofred@users.noreply.github.com>
2023-11-01 05:21:34 -0700

    Update on main page

--- Class_Demos/demo_1_11.py
+++ Class_Demos/demo_1_11.py
@@ -1,5 +1,19 @@
-file = "todaydemo.txt"
-with open(file) as fh:
-    data = fh.read()
-with open(file, 'a') as fh:
-    fh.write(data[::-1])
+tu = ("grace", 43)
+name, age = tu
+empty_t =(4,)
+tu1 =tu[1:]
+tu2 = ("Fred",) + tu1
+"""
+[print(f'{name}--{age}') for i in range(len(tu)) ]"""
+#print(tu2*2)
+#print(tu[1], + tu[0],)
+
+# Named tupple
+
+from collections import namedtuple
+
+employee = namedtuple("record", ["name", "age", "salary"])
+
+e1 = employee("Fred", 33, 1000)
+e1 = employee("grace", 44,20)
+print(employee.count())
