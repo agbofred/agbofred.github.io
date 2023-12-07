94e6005a0aa850da0914a43004114589b0f5354f
agbofred <93585348+agbofred@users.noreply.github.com>
2023-12-07 13:26:58 -0800

    Updating final Questions

--- Questions/F2023_Finals_Questions.py
+++ Questions/F2023_Finals_Questions.py
@@ -2,8 +2,8 @@
 Question 1a.
 
 The Kilburn’s algorithm for finding the factor of a number 
-using brute-force strategy is shown on the right. 
-What would be the printed output of the code below?
+using brute-force strategy is shown below. 
+What output will the code below print?
 """
 def largest_factor(n):
     factor = n - 1
@@ -11,47 +11,61 @@ def largest_factor(n):
         factor -= 1
     return factor
 
-#if __name__ == '__main__':
-    
+if __name__ == '__main__':
+    print(largest_factor(36))
 
     """
-    Options: B
-    --------
-    A= 12
-    B= 18
-    C= 16
-    D= 9  
+    Ans: 18
+    --------  ((2,5,2,5), 'b')
     """
 # -----------------------------------------------
 """
 Q1b:
-What will be printed to the terminal if the following probram 
-execute?
+What output will the following program print?
 """
 # Answer is ((2,5,2,5),'b') >>>>>Note! if line 34 is  a concatenation say C = B + ('b', ), then it returns error of not able to concetnate string and tuple
 A = (2, 5, )
 B = (2*A, ('a', ) )
 C = B ,('b', 'c', 'd')
 D = tuple()
-print(C)
 for v in C[:3]:
     D += v[:1]
 print(D)
 
 """
-2a. If the ASCII code of charater 'G' is 47X in hexadecimal, 
-    what is the base 10 integer value of 'G'?
-ANS:
+2a. If the ASCII code of charater 'G' is represented as 0X47 in hexadecimal, 
+    what is the base 10 integer value representation of 'G'?
+ANS:71
 
-2b. If ASCII code of character 'g' is 67X in hexadecimal, 
+2b. If ASCII code of character 'g' is reprented as OX67 in hexadecimal, 
     what will be the octal representation value of 'g' in base 8?
 
 
-ANS: 
+ANS: 0o147
 """
 #-----------------------------------------------
+
+
+""" 
+Q3a.
+What output will the following program print?
 """
-Q3a. Programming function
+def mystery (x , y =10):
+    z = len (x)
+    return puzzle (x , y) + puzzle (w[:enigma(z ,3)], y )
+def enigma (x , w):
+    return x - w ** 2
+def puzzle (y , z):
+    return y[z :]
+w = "exoticizing nor"
+print (mystery (w , -3))
+
+"""
+Ans: nortic
+"""
+#-----------------------------------------------------
+"""
+Q3b. Function programming
 
 English words that begin and end with the same letter are called "palindromic words." 
 Here are some examples: *Civic*, *noon*, *rotor*. Write a predicate function is_palindromic_word that returns True 
@@ -70,7 +84,7 @@ should return False.
 
 """
 #------------------------------------
-#My solution to Q3a
+#My solution to Q3b
 def is_palindromic_word(word):
     """Returns True if the word contains more than one instance of any letter 
     and the start and end letter of the word are the same irrespective of upper or lower case.
@@ -94,37 +108,35 @@ if __name__ == "__main__":
     #fac = largest_factor(36)
     #print(fac)
 # -----------------------------------------------------------
-""" 
-
-Q3b.
 
 """
+Q4:
+Nowadays, people use emojis to communication in so many ways. This program try to create an emoji that communicate two main things. (1) a smily face and (2) an amazing face.
+You are already provided with the code that draw the face as shown in Figure A.
 
+
+Your task is to write an interactive program that twist the mouth-shape of the emoji to communicate the two scenarios mentioned here.
+Specifically, you are to write two callback functions to achieve these tasks using the GArc or GOval objects,
+
+
+(i). Write a callback function makes_me_smile with a click event that will add a smiley mouth-shape to the face as shown in Figure B.
+
+
+(ii). write a callback function make_me_amaze with a double click event that will add an amazing mouth-shape to the face as shown in Figure 3
 """
-Nowadays, people use emogi to communication in so many ways. This program try to create an emogi that communicate two main things. (1) a smilly face and (2) an amazing face.
-You are already provided with the code that draw the face. Your task is to write an interactive 
-program that twist the mouth-shape of the emogi to communicate the two scenarios mentioned here. 
-Specifically, you are ro write two callback functions to achieve these tasks using the GArc or GOval,
-1. write a callback function makes_me_smile with a click event that will add a smilling mouth-shape to the face as shown in Figure 2. 
-2. write a callback and make_me_amaze with a double click event that will add an amazing mouth-shape to the face as shown in Figure 3
-"""
 
-from pgl import GWindow,GOval, GLine, GArc
+from pgl import GWindow,GOval, GArc
 
-def emogi():
+def emoji():
     gw = GWindow(400, 400)
-    #
-    #Creating the face of the emogi 
-    
-    def face():
+  
+    def face(): #Creating the face of the emogi 
         head = GOval(20, 20, 360, 360)
         head.set_fill_color("yellow")
         head.set_filled(True)
         return gw.add(head)
     
-    #Creating the right hand eye of the emogi 
-    
-    def righteye():
+    def righteye(): #Creating the right hand eye of the emogi 
         reye = GOval(110, 100, 40, 40)
         reye.set_filled(True)
         gw.add(reye)
@@ -133,9 +145,7 @@ def emogi():
         rdoteye.set_filled(True)
         return gw.add(rdoteye)
     
-    #Creating the left hand eye of the emogi 
-    
-    def lefteye():
+    def lefteye(): #Creating the left hand eye of the emogi 
         leye = GOval(250, 100, 40, 40)
         leye.set_filled(True)
         gw.add(leye)
@@ -145,35 +155,43 @@ def emogi():
         gw.add(ldoteye)
         return gw
     
-    #Your smile mouth-shape callback function  
-    
-    def makes_me_smile(event):
+    def make_me_smile(event): #Your smile mouth-shape callback function  
+        """
+        Type in your code in this bluck
+        """
+        """
         smile = GArc(150, 250, 110, 50, 180, 180)
         smile.set_line_width(4)
         gw.add(smile)
         return gw
-    gw.add_event_listener("click", makes_me_smile) 
+    gw.add_event_listener("click", make_me_smile) 
+    """
     
-    #Your amazing mouth-shape callback function  
-    
-    def make_me_amaze(event):
+    def make_me_amaze(event): #Your amazing mouth-shape callback function  
+        """
+        Type in your code in this bluck
+        """
+        """
         amaze = GArc(150, 250, 110, 50, 0, 180)
         amaze.set_line_width(4)
         gw.add(amaze)
         return gw
     gw.add_event_listener("dblclick", make_me_amaze)
+    """
     
     #Instantiating the objects
     face()
     righteye()
     lefteye()
 
-
 if __name__ == "__main__":
-    emogi() # Runing theh emogi program
+    emoji() # Runing theh emogi program
 
     # ----------------------------------------------------
-
+"""
+Q5.
+Consider the data structure below which somewhat indicates the calendar for the remaining weeks in the year. 
+"""
 
 data = [
         {'week': 50,
@@ -210,19 +228,58 @@ data = [
         }
     }
 ]
-"""Question A: to select and print only the Christmas Holiday..."""
+"""Question A: write python expression to select and print only the value with 'Christmas holiday'."""
 
-print(data[2]['days']['Mon'])
+# ANS: print(data[2]['days']['Mon'])
 
+# --------------------------------------------------------
 
-""" Question B: to format the data and print to terminal as shown in the example"""
+""" Question B: Write a python program to format the data and print to terminal as shown in the example below
+
+Week: 50 
+--------
+Mon: Workday
+Tue: Workday
+Wed: Workday
+Thu: Workday
+Fri: Workday
+Sat: Weekend
+Sun: Weekend
+
+Week: 51 
+--------
+Mon: Workday
+Tue: Workday
+Wed: Workday
+Thu: Workday
+Fri: Workday
+Sat: Weekend
+Sun: Christmas eve
+
+Week: 52 
+--------
+Mon: Christmas holiday
+Tue: Boxing day holiday
+Wed: Workday
+Thu: Workday
+Fri: Workday
+Sat: Weekend
+Sun: New year eve
+
+"""
+# My solution to question B
 for i in range(len(data)):
     print(f"Week: {data[i]['week']} \n--------")
     for x,y in data[i]['days'].items():
         print(f'{x}: {y}')
     print()
 
-"Question C: to save this information to a file called End_of_year_calendar.txt"
+# ----------------------------------------------------------------
+
+"""Question C: Write python program to store the formatted data structure in question C into 
+the local computer memory with filename 'End_of_year_calendar.txt' """
+
+#My solution to question C
 with open("End_of_year_calendar.txt", "w") as fh:
             for i in range(len(data)):
                 fh.write(f"Week: {data[i]['week']} \n--------\n")
