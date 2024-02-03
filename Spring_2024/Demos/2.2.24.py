5af717fdf6485a2331f9135cecfac70b3e21be4c
agbofred <93585348+agbofred@users.noreply.github.com>
2024-02-03 15:20:16 -0800

    Update 2.2.24.py

--- Spring_2024/Demos/2.2.24.py
+++ Spring_2024/Demos/2.2.24.py
@@ -1,18 +1,13 @@
 def count_letters(letter, string):
-		count = 0
-		for character in string:
-			if character == letter:
-				count += 1
-		return count
+    count = 0
+    for character in string:
+        if character == letter:
+            count += 1
+    return count
 
 def test_count_letters():
-	""" Runs several tests on the function count_letters """
-	assert count_letters("a", "banana") == 3
-	assert count_letters("a", "strawberry") == 1
-	assert count_letters("A", "apple") == 0
-	assert count_letters("e", "eerie") == 3
-
-if __name__ == '__main__':
-	#print(count_letters("e", "elephants ego"))
-	test_count_letters()
-	
+    """ Runs several tests on the function count_letters """
+    assert count_letters("A", "banana") == 3
+if __name__ =='__main__':
+    print(count_letters("3", "alexa"))
+    test_count_letters()
