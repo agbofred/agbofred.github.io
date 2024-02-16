2338df5bc1c815bb1f93dfdf6d72e8147958513b
agbofred <93585348+agbofred@users.noreply.github.com>
2024-02-16 03:45:26 -0800

    Update of Ch5 slides

--- Spring_2024/Demos/sandbox.py
+++ Spring_2024/Demos/sandbox.py
@@ -1,7 +1,14 @@
 from pgl import GWindow, GOval, GLine, GRect, GLabel
-gw = GWindow(500, 200)
-msg = GLabel("hello world!")
-msg.set_font("italic bold 20px 'times new roman'")
-x = 250 - msg.get_width() / 2
-y = 100 + msg.get_ascent() / 2
-gw.add(msg, x, y)
+
+def make_filled_circ(x_cent, y_cent, radius, color='black'):
+    circle = GOval(x_cent-radius, y_cent-radius, 2*radius, 2*radius)
+    circle.set_color(color)
+    circle.set_filled(True)
+    return circle
+
+if __name__ =="__main__":
+    gw = GWindow(800, 600)
+    c1= make_filled_circ(200,200, 20,"green")
+    gw.add(c1)
+    c2= make_filled_circ(300,300, 40,"red")
+    gw.add(c2)
