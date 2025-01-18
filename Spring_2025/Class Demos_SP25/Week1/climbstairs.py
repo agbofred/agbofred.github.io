1b26ac377dae9ebace4efad4739f5ca2925e460d
agbofred <93585348+agbofred@users.noreply.github.com>
2025-01-17 16:54:09 -0800

    Update ch03

--- Spring_2025/Class Demos_SP25/Week1/climbstairs.py
+++ Spring_2025/Class Demos_SP25/Week1/climbstairs.py
@@ -1,4 +1,16 @@
 import karel
 
 def main():
-    pass
+  while front_is_blocked():
+            if facing_east():
+                turn_left()
+            if front_is_clear():
+                step_up()
+def step_up(): 
+    move()
+    turn_right()
+    move()
+    put_beeper()
+def turn_right():
+     for i in range(3):
+          turn_left()
