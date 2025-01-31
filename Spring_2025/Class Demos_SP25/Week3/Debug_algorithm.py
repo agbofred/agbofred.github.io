734a34a3b804671e6798d010d5554a5d281af13f
agbofred <93585348+agbofred@users.noreply.github.com>
2025-01-31 04:46:29 -0800

    Ch 1 - 3 class activities update

--- Spring_2025/Class Demos_SP25/Week3/Debug_algorithm.py
+++ Spring_2025/Class Demos_SP25/Week3/Debug_algorithm.py
@@ -1,1 +1,9 @@
-print("hellow" + 3)
+def greatest_factor(num):
+    """Finds the greatest factor of a number."""
+    for i in range(num-1,0,-1):
+        if num % i == 0:
+            return i
+        
+if __name__ =='__main__':
+    print(greatest_factor(46))
+    #print(71*2)
