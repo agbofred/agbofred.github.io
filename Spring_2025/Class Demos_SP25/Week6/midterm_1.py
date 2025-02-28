f86dcc17c9f16edee8f64d09d01e45fc5b00585b
agbofred <93585348+agbofred@users.noreply.github.com>
2025-02-28 06:24:26 -0800

    Chpt 6_3 update

--- Spring_2025/Class Demos_SP25/Week6/midterm_1.py
+++ Spring_2025/Class Demos_SP25/Week6/midterm_1.py
@@ -1,20 +1,37 @@
 from english import ENGLISH_WORDS, is_english_word
+import random
 
-def one_off (x):
-    count = 0
-    y = ""
-    z = "abcdefghijklmnopqrstuvwxyz"
-    for i in range(len(x)):
-        for j in range(len(z)):
-            for k in range(len(x)):
-                if k == i:
-                    y += z[j]
-                else:
-                    y += x[k]
-            if not(x[i] == y[i]):
-                if y[i] in ENGLISH_WORDS:
-                    print(y)
-                    count += 1
-    return count
-                    
-    print(one_of("word"))
+def one_off(real_word):
+
+ #first i make an empty list to put words that are the correct length into
+
+    word_list = []
+
+    final_words = []
+
+    for words in ENGLISH_WORDS:
+
+        if len(words) == len(real_word):
+
+            word_list.append(words)
+
+ #now i take my real word and make it missing one letter and checking if this
+
+ #matches another word missing a letter in the same index
+
+            for x in range(len(real_word)):
+
+                fake_word = real_word[:x] + real_word[x+1:]
+
+                for word in word_list:
+
+                    for z in range(len(word)):
+
+                        other_word = word[:z] + word[z+1:]
+
+                        if fake_word == other_word:
+
+                            word_list += word
+
+                            print(word_list)
+print(one_off("grace"))
