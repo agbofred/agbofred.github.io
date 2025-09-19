36ba334e858278c2c61711690c46980cbd037c24
agbofred <93585348+agbofred@users.noreply.github.com>
2025-09-19 02:53:45 -0700

    PS2 update

---
+++ Fall_2025/Homeworks/ps2/starter/ps2_code.py
@@ -0,0 +1,22 @@
+# ps2_code.py
+########################################
+# Your Name:
+# Indicate any collaborators or tools used to assist you including AI:
+# Estimated time spent (hrs):
+########################################
+
+"""
+Starter file for Problem Set 2 code snippets.
+Add your function/class definitions and any code required for the assignment here.
+Each function should be clearly labeled with a comment indicating which question it answers.
+"""
+
+# Example for Part 2, Question 3a:
+def custom_insert(arr_data, logical_size, target_index, new_item, default_capacity):
+    """
+    Simulate insertion into a fixed-size array.
+    Complete this function as described in the assignment.
+    """
+    pass  # TODO: Implement your solution here
+
+# Add more functions/classes as needed for other questions below.
