cd2303609d088e4651fe406a37685095c82f4cc5
agbofred <93585348+agbofred@users.noreply.github.com>
2026-01-27 17:04:14 -0800

    Ch3.2 update

--- DS_Spring_2026/Demos_SP25/Week3/class_demo.py
+++ DS_Spring_2026/Demos_SP25/Week3/class_demo.py
@@ -1,5 +1,19 @@
 for n in range(40):
     print(f"{n}--> {n**4} ----------- {2**n}")
+    
+    
+    
+#FIbonnacci
+
+def fib(n):
+    if n<=1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+for i in range(10):  
+    print(fib(i))
+    
+
 
 # Recursive function to reverse string or list
 def reverseString(s):
@@ -21,4 +35,14 @@ def recursiveFind(x, num, index=0):
     return recursiveFind(x, num, index + 1)
 cool=[4,6,3,4,6,10]    
 print(recursiveFind(11, cool))
+
+
+
+def checkPowerofN(n):
+    print(f"{'#':<10}  {'Problem size':<20} {'Computation':>10}")
+    print(f"{'----------'}{'------------':<20} {'---------':>10}")
+    for i in range (20):
+        print(f"{i:<10}{i**4:<20} {2**i:>10}")
+checkPowerofN(20)     
+        
         
